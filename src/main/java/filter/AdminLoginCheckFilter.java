63b633e520fc84d3b1321c228f1619b145a2e3a9
WangZhiqiang <303125385@qq.com>
2018-07-21 09:17:03 +0800

    add init() and destroy()

--- src/main/java/filter/AdminLoginCheckFilter.java
+++ src/main/java/filter/AdminLoginCheckFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -13,7 +14,20 @@ import javax.servlet.http.HttpServletResponse;
 import utils.Util;
 
 public class AdminLoginCheckFilter implements Filter {
+    
+    
+    
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
 
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         String aid = (String)((HttpServletRequest)request).getSession().getAttribute("aid");
--- src/main/java/filter/LoginCheckFilter.java
+++ src/main/java/filter/LoginCheckFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -15,6 +16,19 @@ import utils.Util;
 
 public class LoginCheckFilter implements Filter {
 
+    
+    
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         String uid = (String)((HttpServletRequest)request).getSession().getAttribute("uid");
